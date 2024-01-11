7c0af4348c1fdc3e4ceace430ff3578dc0e521b7
rshaki01 <rshaki01@gmail.com>
2024-01-10 23:37:00 -0500

    change

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import  Cat  from './components/Cat';
+import  CircleProgress  from './components/CircleProgress';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar style="" />
-      <Cat/>
-      <Image
-        source={{
-          uri: 'https://reactnative.dev/docs/assets/p_cat1.png',
-        }}
-        style={{width: 201, height: 200}}
-      />
+      <CircleProgress progress={4}/>
     </View>
   );
 }
---
+++ components/CircleProgress.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react'
+import { View } from 'react-native';
+import Svg, { Circle, Text } from 'react-native-svg';
+
+const CircleProgress = ({ progress }) => {
+
+    const [circumference, setCircumference] = useState(0);
+
+    useEffect(() => {
+        const radius = 50; // Change this based on your desired size
+        const calculatedCircumference = 2 * Math.PI * radius;
+        setCircumference(calculatedCircumference);
+      }, []);
+
+
+  return (
+    <View>
+        <Svg width="200" height="200">
+            <Circle
+                cx="100"
+                cy="100"
+                r="50"
+                fill="transparent"
+                stroke="#d3d3d4"  // Change this to the progress color
+                strokeWidth="30"
+                stroke-dasharray="439.8"
+                stroke-dashoffset="0"
+            />
+            <Circle
+                cx="100"
+                cy="100"
+                r="50"
+                fill="transparent"
+                stroke="#389dff"  // Change this to the progress color
+                strokeWidth="30"
+                stroke-dasharray="439.8"
+                stroke-dashoffset="66"
+            />
+            <Text x="50%" y="50%" dominant-baseline="central" text-anchor="middle">85%</Text>
+
+        </Svg>
+    </View>
+  )
+}
+
+export default CircleProgress
